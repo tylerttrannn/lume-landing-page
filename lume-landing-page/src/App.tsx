e2f4032ddf33af28dd69891f0b3e363523a44565
Tyler Tran <tylerttrann@gmail.com>
2025-09-15 13:51:57 -0700

    added basic footer

--- lume-landing-page/src/App.tsx
+++ lume-landing-page/src/App.tsx
@@ -1,13 +1,25 @@
 
 import Navbar from "../src/components/comp-577"
-
+import Footer from "../src/components/sections/footer/Footer"
 
 
 function App() {
 
   return (
-    <div>
+    <div className = "flex flex-col min-h-screen">
       <Navbar/>
+
+      {/* Screneshot / main attracion part*/}
+
+      {/* features cards */}
+
+      {/*Footer componetn */} 
+
+      <div className ="absolute bottom-0 left-0 right-0">
+        <Footer/>
+      </div>
+
+
       <h1>Test</h1>
     </div>
 
